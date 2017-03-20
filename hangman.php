08e1d7f44a26174163fe5fdc68a2a267fd468c17
chavdar00 <chavdargoranov00@gmail.com>
2017-03-20 13:27:44 +0200

    Update hangman.php

--- hangman.php
+++ hangman.php
@@ -1,6 +1,11 @@
+<?php
+session_start();
+include('db_connect.php');
+
+?>
 <html>
 <head>
-  <title></title>
+  <title>Game</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
   <link rel="stylesheet" type="text/css" href="form_css.css">
@@ -10,720 +15,15 @@
 <body bgcolor="#ffffff" link="navy" vlink="navy" alink="navy">
 
   <?php
-  $list = "xenophobia
-  xenophobic
-  aback
-  abacus
-  abandon
-  abandoned
-  abandonment
-  abashed
-  abate
-  abbey
-  abbr.
-  abbreviate
-  abbreviation
-  ABC
-  ABC's
-  abdicate
-  abdication
-  abdomen
-  abdominal
-  abduct
-  abduction
-  aberration
-  abet
-  abhor
-  abhorrence
-  abhorrent
-  abide
-  abiding
-  ability
-  abject
-  ablaze
-  able
-  able-bodied
-  ably
-  abnormal
-  abnormality
-  abnormally
-  aboard
-  abolish
-  abolition
-  abolitionist
-  abominable
-  aboriginal
-  aborigine
-  abort
-  abortion
-  abortive
-  abound
-  about
-  about-face
-  above
-  aboveboar
-  brotherhood
-  brother-in-law
-  brotherly
-  brothers-in-law
-  brought
-  brow
-  browbeat
-  brown
-  Brownie
-  brownie
-  Brownies
-  brownish
-  brownstone
-  browse
-  browser
-  colt
-  column
-  columnist
-  coma
-  comatose
-  comb
-  combat
-  combatant
-  combative
-  combination
-  combine
-  combined
-  combustible
-  combustion
-  checkerboard
-  checkered
-  checkers
-  check-in
-  checking account
-  checklist
-  check mark
-  checkmate
-  checkout
-  checkpoint
-  checkup
-  cheddar
-  cheek
-  cheekbone
-  cheer
-  cheerful
-  domestic
-  domesticate
-  domesticated
-  domesticity
-  domicile
-  dominance
-  dominant
-  dominate
-  domination
-  domineering
-  dominion
-  domino
-  dominoes
-  donate
-  donation
-  done
-  donkey
-  emergence
-  emergency
-  emergency brake
-  emergency room
-  emergent
-  emeritus
-  emery board
-  emigrant
-  emigrate
-  emigration
-  eminent
-  eminently
-  emir
-  emirate
-  emissary
-  emission
-  emit
-  Emmy
-  emotion
-  emotional
-  emotionally
-  freely
-  free market
-  free-range
-  free speech
-  freethinker
-  freethinking
-  freeway
-  freewheeling
-  free will
-  freeze
-  freeze-dried
-  freezer
-  freezing
-  freezing point
-  freight
-  freighter
-  freight train
-  French
-  French bread
-  French fry
-  French toast
-  frenetic
-  frenzied
-  frenzy
-  grammar school
-  grammatical
-  grammatically
-  Grammy
-  grand
-  grandchild
-  grandchildren
-  granddad
-  granddaughter
-  grandeur
-  grandfather
-  grandfather clock
-  grandiose
-  grand jury
-  grandly
-  grandma
-  grandmother
-  grandpa
-  grandparent
-  grand piano
-  grand prix
-  hairdo
-  hairdresser
-  hair dryer
-  hairline
-  hairnet
-  hairpiece
-  hair-raising
-  hairsplitting
-  hair spray
-  hairstyle
-  hairstylist
-  hairy
-  hale
-  half
-  half-and-half
-  half-assed
-  half-baked
-  half brother
-  halfhearted
-  halfheartedly
-  half-mast
-  half sister
-  interact
-  interaction
-  interactive
-  intercede
-  intercept
-  interception
-  interchange
-  interchangeable
-  interchangeably
-  intercom
-  interconnect
-  interconnected
-  intercontinental
-  intercourse
-  interdependence
-  interdependent
-  interest
-  interested
-  interesting
-  interestingly
-  interface
-  interfere
-  interference
-  jovial
-  jowls
-  joy
-  joyful
-  joyfully
-  joyfulness
-  joyous
-  joyously
-  joyride
-  joyrider
-  joyriding
-  joystick
-  knitting needle
-  knives
-  knob
-  knobby
-  knock
-  knockdown-dragout
-  knocker
-  knockout
-  knoll
-  knot
-  knotty
-  know
-  know-how
-  knowing
-  knowingly
-  know-it-all
-  knowledge
-  knowledgeable
-  knowledgeably
-  known
-  knuckle
-  lumberjack
-  lumberyard
-  luminary
-  luminous
-  lump
-  lump sum
-  lumpy
-  lunacy
-  lunar
-  lunatic
-  lunch
-  lunchbox
-  luncheon
-  lunchtime
-  lung
-  lunge
-  lurch
-  lure
-  lurid
-  luridly
-  lurk
-  luscious
-  lush
-  lust
-  luster
-  lustrous
-  lusty
-  Lutheran
-  luxuriant
-  luxuriate
-  luxurious
-  luxuriously
-  luxury
-  vertically
-  vertigo
-  verve
-  very
-  vessel
-  vest
-  vestibule
-  vestige
-  vet
-  veteran
-  veterinarian
-  veterinary
-  veto
-  vex
-  via
-  viability
-  viable
-  viaduct
-  vial
-  ed
-  unusual
-  unusually
-  unveil
-  unwanted
-  unwarranted
-  unwelcome
-  unwieldy
-  unwilling
-  unwind
-  unwise
-  unwitting
-  unwittingly
-  unwound
-  unwritten
-  unyielding
-  unzip
-  up
-  up-and-coming
-  upbeat
-  upbringing
-  upchuck
-  upcoming
-  update
-  upend
-  upfront
-  upgrade
-  upheaval
-  upheld
-  uphill
-  uphold
-  upholster
-  upholstered
-  upholstery
-  tee
-  teem
-  teen
-  teenage
-  teenaged
-  teenager
-  teens
-  teeny-weeny
-  teeter
-  teeter-totter
-  teeth
-  teethe
-  teetotaler
-  Teflon
-  telecommunications
-  telecommuter
-  telecommuting
-  telegram
-  telegraph
-  telepathy
-  stopwatch
-  storage
-  store
-  storehouse
-  storekeeper
-  storeroom
-  storey
-  stork
-  storm
-  stormy
-  story
-  storyteller
-  stout
-  stove
-  stow
-  stowaway
-  straddle
-  straggle
-  straight
-  straighten
-  straightforward
-  strain
-  strained
-  strainer
-  reconcile
-  reconciliation
-  recondition
-  reconnaissance
-  reconsider
-  reconstitute
-  reconstruct
-  reconstruction
-  record
-  record-breaking
-  recorder
-  recording
-  record player
-  recount
-  recoup
-  recourse
-  recover
-  recovery
-  recreate
-  recreation
-  recreational
-  queer
-  quell
-  quench
-  query
-  quest
-  question
-  questionable
-  question mark
-  questionnaire
-  pristine
-  privacy
-  private
-  private enterprise
-  private investigator
-  privately
-  private parts
-  privation
-  privatization
-  privatize
-  privilege
-  privileged
-  privy
-  prize
-  pro
-  probability
-  probable
-  probably
-  probation
-  probation officer
-  probe
-  probing
-  problem
-  problematic
-  procedural
-  procedure
-  proceed
-  proceedings
-  proceeds
-  overthrown
-  overtime
-  overtly
-  overtone
-  overtook
-  overture
-  overturn
-  overview
-  overweight
-  overwhelm
-  overwhelmed
-  overwhelming
-  overwhelmingly
-  overwork
-  overworked
-  overwrought
-  owing to
-  noisy
-  nomad
-  nomadic
-  nomenclature
-  nominal
-  nominally
-  nominate
-  nomination
-  nominee
-  nonchalance
-  nonchalant
-  nonchalantly
-  noncommittal
-  noncommittally
-  nonconformist
-  nondairy
-  nondenominational
-  nondescript
-  malted milk
-  mama
-  mammal
-  mammoth
-  man
-  manacle
-  manage
-  manageable
-  management
-  manager
-  managerial
-  Mandarin
-  mandarin
-  Mandarin Chinese
-  mandarin orange
-  mandate
-  mandatory
-  mane
-  maneuver
-  maneuvers
-  mange
-  manger
-  mangle
-  mango
-  mangrove
-  mangy
-  manhandle
-  manhole
-  manhood
-  manhunt
-  mania
-  maniac
-  maniacal
-  manic
-  manicure
-  manicurist
-  manifest
-  manifestation
-  manifesto
-  manifold
-  manipulate
-  manipulation
-  manipulative
-  mankind
-  manliness
-  manly
-  man-made
-  manned
-  mannequin
-  manner
-  mannerism
-  manners
-  mannish
-  manor
-  manpower
-  mansion
-  manslaughter
-  mantel
-  mantelpiece
-  mantle
-  mantra
-  manual
-  manually
-  manufacture
-  manufacturer
-  manufacturing
-  manure
-  manuscript
-  many
-  Maori
-  map
-  maple
-  mar
-  Mar.
-  marathon
-  marble
-  marbles
-  March
-  march
-  Mardi Gras
-  mare
-  margarine
-  margin
-  marginal
-  marginally
-  margin of error
-  marijuana
-  marina
-  marinade
-  marinate
-  Marine
-  marine
-  Marine Corps
-  mariner
-  marionette
-  marital
-  marital status
-  maritime
-  mark
-  markdown
-  marked
-  marker
-  market
-  marketability
-  marketable
-  marketer
-  marketing
-  marketplace
-  market research
-  marking
-  marksman
-  markup
-  marmalade
-  maroon
-  marooned
-  marquee
-  marriage
-  married
-  marrow
-  marry
-  Mars
-  marsh
-  marshal
-  marshmallow
-  marshy
-  marsupial
-  mart
-  martial
-  martial art
-  martial law
-  Martian
-  martyr
-  martyrdom
-  marvel
-  marvelous
-  Marxism
-  Marxist
-  masc.
-  mascara
-  mascot
-  masculine
-  masculinity
-  mash
-  mask
-  masking tape
-  masochism
-  masochist
-  masochistic
-  Mason
-  mason
-  masonry
-  masquerade
-  Mass
-  mass
-  massacre
-  massage
-  masses
-  masseur
-  masseuse
-  massive
-  mass media
-  mass-produce
-  mass production
-  mast
-  master
-  masterful
-  master key
-  mastermind
-  master of ceremonies
-  masterpiece
-  master plan
-  mastery
-  masturbate
-  masturbation
-  mat
-  match
-  matchbook
-  matchbox
-  matchless
-  matchmaker
-  matchmaking
-  matchstick
-  mate
-  material
-  materialism
-  materialist
-  materialistic
-  materialize
-  maternal
-  maternity
-  math
-  mathematical
-  mathematically
-  mathematician
-  mathematics
-  matinee
-  matriarch
-  matriarchal
-  matriarchy
-  matriculate
-  matrimonial
-  matrimony
-  matron
-  matronly
-  matron of honor
-  matte
-  matted
-  matter
-  matting
-  mattress
-  mature
-  maturity
-  maudlin
-  maul
-  mausoleum
-  mauve
-  maverick
-  mawkish
-  maxim
-  maximize
-  maximum
-  May
-  maybe
-  mayday
-  May Day
-  mayh
-  ";
+ include('words.php');
 
+    echo "<form action='hangman.php' method='post'>";
   $alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   $additional_letters = " -.,;!?%&0123456789";
   $len_alpha = strlen($alpha);
-
+  $total_games=$_SESSION['total_games'];
+  $won_games=$_SESSION['won_games'];
+  $email=$_SESSION['email'];
   if(isset($_GET["n"])) $n=$_GET["n"];
   if(isset($_GET["letters"])) $letters=$_GET["letters"];
   if(!isset($letters)) $letters="";
@@ -733,9 +33,9 @@
 
   $links="";
 
-  $max=6;					
-  $list = strtoupper($list);
-  $words = explode("\n",$list);
+  $max=6;         
+  $list = strtoupper($list);//converts a string to uppercase
+  $words = explode("\n",$list);// breaks a string into an array
   srand ((double)microtime()*1000000);
   $all_letters=$letters.$additional_letters;
   $wrong = 0;
@@ -744,7 +44,7 @@
 
   if (!isset($n)) { $n = rand(1,count($words)) - 1; }
   $word_line="";
-  $word = trim($words[$n]);
+  $word = trim($words[$n]);//removes whitespace and other predefined characters from both sides of a string
   $done = 1;
   for ($x=0; $x < strlen($word); $x++)
   {
@@ -768,9 +68,8 @@
       else
         { $links .= "\n<a href=\"$self?letters=$alpha[$c]$letters&n=$n\">$alpha[$c]</a> "; }
     }
-    $nwrong=$wrong; if ($nwrong>6) $nwrong=6;
+    $nwrong=$wrong; if ($nwrong>6) $nwrong=6;//displaing img
     echo "\n<p><br>\n<img src=\"img/hangman_$nwrong.gif\" align=\"middle\" border=0 width=100 height=100 alt=\"Wrong: $wrong out of $max\">\n";
-
     if ($wrong >= $max)
     {
       $n++;
@@ -780,27 +79,49 @@
       if (strstr($word, " ")) $term="phrase"; else $term="word";
       echo "The $term was \"<b>$word</B>\"<br><br>\n";
 
-      echo "<a href=$self?n=$n class='btn btn-lg btn-primary btn-block signup-btn' style='text-decoration:none'>Play again.</a>\n\n";
-   
-    }
-    else
-    {
-      echo "# Wrong Guesses Left: <b>".($max-$wrong)."</b><br>\n";
-      echo "<h1><font size=5>\n$word_line</font></h1>\n";
-      echo "<p><br>Choose a letter:<br><br>\n";
-      echo "$links\n";
-    }
-  }
-  else
-  {
+     
+ echo "<button location.href='hangman.php' type='submit' class='btn btn-lg btn-primary btn-block signup-btn'>Play againg</button>";//play again button
+  echo "<input type='submit' class='btn btn-lg btn-primary btn-block signup-btn' style='text-decoration:none' name='submit' value='Go back'>\n\n";
+
+  $total_games=$total_games+1;
+  $_SESSION['total_games']=$total_games;
+}
+else
+{
+  echo "# Wrong Guesses Left: <b>".($max-$wrong)."</b><br>\n";
+  echo "<h1><font size=5>\n$word_line</font></h1>\n";
+  echo "<p><br>Choose a letter:<br><br>\n";
+  echo "$links\n";
+}
+}
+else
+{
   $n++;	// get next word
   if ($n>(count($words)-1)) $n=0;
+  $total_games=$total_games+1;
+  $_SESSION['total_games']=$total_games;
+  $won_games=$won_games+1;
+  $_SESSION['won_games']=$won_games;
+
   echo "<br><br><h1><font size=5>\n$word_line</font></h1>\n";
   echo "<p><br><br><b>Congratulations!!!<h2>You win!!!</h2></b><br><br><br>\n";
-  echo "<a href=$self?n=$n  class='btn btn-lg btn-primary btn-block signup-btn' style='text-decoration:none'>Play again</a>\n\n";
- 
+  
+  echo "<button location.href='hangman.php' type='submit' class='btn btn-lg btn-primary btn-block signup-btn'>Play againg</button>";//play again button
+  echo "<input type='submit' class='btn btn-lg btn-primary btn-block signup-btn' style='text-decoration:none' name='submit' value='Go back'>\n\n";//back to login1.php
+
 }
-echo "<a href='logout.php'  class='btn btn-lg btn-primary btn-block signup-btn' style='text-decoration:none'>Log out</a>\n\n";
+echo "</form>";
+if (isset($_POST['submit'])) {
+  $query="UPDATE `users` SET `total_games`='$total_games',`won_games`='$won_games' WHERE `email` = '$email'";
+  $result=mysqli_query($conn,$query);
+  header('Location:login1.php');
+
+  echo $total_games;
+  echo $won_games;
+  
+}
+
+
 ?>
 </body>
 <html>
